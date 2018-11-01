87f27c0bb5fd8d7e490643aefeea610947511645
Chitre <schitre@paypal.com>
2018-10-31 23:03:31 -0700

    initial weather app commit

---
+++ src/containers/search-bar.js
@@ -0,0 +1,38 @@
+import React, {Component} from 'react';
+
+export default class SearchBar extends Component {
+    constructor(props){
+        super(props);
+        this.state = {term: ''};
+        this.onInputChange = this.onInputChange.bind(this);
+
+        //else this is undefined in onInputChange
+
+        //if you have a callback that references this, check that you may have to bind this
+    }
+
+    onInputChange(event) { //all dom handlers come with event object dom feature
+        this.setState({term: event.target.value});
+    }
+
+    onFormSubmit(event) {
+        event.preventDefault();
+        //we need to go and fetch weather data
+    }
+
+    render(){
+        return (
+            <form onSubmit={this.onFormSubmit} className="input-group">
+                <input
+                    placeholder="Get a five-day forecast in your favorite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
+                <span className="input-group-btn">
+                    <button type="submit" className="btn btn-secondary">Submit</button>
+                </span>
+            </form>
+        );
+    }
+}
