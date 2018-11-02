cb264b5b7581020ec8a4d7ac73ddd11277c97db6
Chitre <schitre@paypal.com>
2018-11-01 23:11:38 -0700

    add ajax request using axios and import redux promise

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
-const API_KEY= 'ZZZ';
+import axios from 'axios';
+
+const API_KEY= '90f180c59ebb7e3e818e822284643151';
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+//action creator returns an action - an object which always needs to have a type
+export function fetchWeather(city){ //city is the search term
+    const url = `${ROOT_URL}&q=${city},us`; //we are making country code static in this applicaition
+    
+    const request = axios.get(url); //this will return a promise
+
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request //payload is an optional additional property that contains additional data that describes the action
+    }
+}
