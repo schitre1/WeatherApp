558a7c86f8a100989b3f46ca486ab4f995bbd464
Chitre <schitre@paypal.com>
2018-11-05 22:58:24 -0800

    Add chart details and pressure, humidity and separate chart as a resuable component

--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
-import {Sparklines, SparklinesLine} from 'react-sparklines';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
 
     renderWeather(cityData){
         const name = cityData.city.name;
 
-        const temps = cityData.list.map( weather => weather.main.temp);
+        const temps = cityData.list.map( weather => weather.main.temp); //if you want to convert temp to different scale, can do that right here
         const pressures = cityData.list.map( weather => weather.main.pressure);
         const humidities = cityData.list.map( weather => weather.main.humidity);
         console.log(temps);
@@ -16,9 +16,9 @@ class WeatherList extends Component {
         return (
             <tr key={name}>
                 <td>{name}</td>
-                <Sparklines height={120} width={180} data={temps}>
-                    <SparklinesLine color = "red"/>
-                </Sparklines>
+                <td><Chart data={temps} color="orange" units="K" /></td>
+                <td><Chart data={pressures} color="blue" units="hPa" /></td>
+                <td><Chart data={humidities} color="black" units="%" /></td>
             </tr>
         )
     }
@@ -28,10 +28,10 @@ class WeatherList extends Component {
             <table className="table table-hover">
                 <thead>
                     <tr>
-                        <th>City</th>
-                        <th>Temperature</th>
-                        <th>Pressure</th>
-                        <th>Humidity</th>
+                        <th>City Name</th>
+                        <th>Temperature (K)</th>
+                        <th>Pressure  (hPa)</th>
+                        <th>Humidity  (%)</th>
                     </tr>
                 </thead>
                 <tbody>
